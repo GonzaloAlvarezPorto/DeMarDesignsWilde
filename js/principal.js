e262dcbe8ba4771dd8cd987e3ab9791cb267a1cb
Gonzalo Alvarez Porto <gonzaalvarezporto@gmail.com>
2024-03-11 15:12:53 -0300

    Correciones en la galeria de proyectos

--- js/principal.js
+++ js/principal.js
@@ -8,4 +8,31 @@ abrir.addEventListener("click", () => {
 
 cerrar.addEventListener("click", () => {
     nav.classList.remove("visible");
-})
+});
+
+let currentProjectSlide = 0;
+
+function cambiarFotoProyecto(n) {
+    mostrarFotoProyecto(currentProjectSlide += n);
+}
+
+function mostrarFotoProyecto(n) {
+    const slides = document.querySelector('.proyectos__general').children;
+
+    if (n >= slides.length) {
+        currentProjectSlide = 0;
+    } else if (n < 0) {
+        currentProjectSlide = slides.length - 1;
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+
+    slides[currentProjectSlide].style.display = 'flex';
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    mostrarFotoProyecto(currentProjectSlide);
+});
+
